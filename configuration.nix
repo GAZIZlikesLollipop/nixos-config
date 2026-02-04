a835266b381dfc8e9d47559145667c7c261cd13c
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-02-04 21:48:42 +0500

    comment fixes

--- configuration.nix
+++ configuration.nix
@@ -300,13 +300,15 @@ in
     }; 
   };
 
-  # Graphical drivers (менять в зависимости от железа!) 
+  # Конфигурация зависящаяя от железа (в данном случае laptop)
+  # Graphical drivers
   nixpkgs.config.allowUnfree = true;
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
+    powerManagement.finegrained = false;
     open = false; 
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
